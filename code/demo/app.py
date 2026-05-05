e0fac352dd3e4e2fe99f7ffa8ca2c44ffb022cf1
marmag0 <mikolaj.mazur05@gmail.com>
2026-05-05 18:50:15 +0200

    demo naming convention changed

--- code/demo/app.py
+++ code/demo/app.py
@@ -247,7 +247,7 @@ def init_model(conversation_type: str = "human_in_the_loop", system_prompt: str 
 
 
 if __name__ == "__main__":
-    init_model(conversation_type="human_in_the_loop", system_prompt="system_eval.xml", initial_user="user")
+    init_model(conversation_type="human_in_the_loop", system_prompt="system_eval_demo.xml", initial_user="user")
     #init_model(conversation_type="automated_test", system_prompt="system_eval.xml", test_file="tests/01_schema_adherence.txt", initial_user="user")
     #init_model(conversation_type="automated_test", system_prompt="system_eval.xml", test_file="tests/02_persona_adoption.txt", initial_user="user")
     #init_model(conversation_type="automated_test", system_prompt="system_eval.xml", test_file="tests/03_alignment_tax.txt", initial_user="user")
